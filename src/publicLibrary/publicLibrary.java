dc9dc3ea52116314e91e9707a2a5e560df4123ae
Jan Pytlarczyk <69763848+JanPytlarczyk@users.noreply.github.com>
2021-02-23 18:55:39 +0100

    add first

---
+++ src/publicLibrary/publicLibrary.java
@@ -0,0 +1,13 @@
+package publicLibrary;
+
+import java.util.Arrays;
+
+public class publicLibrary extends addUserToLibrary {
+
+        public static void main(String[] args) {
+
+
+                System.out.println("Pojedyńczy user to: "+ nowy1.imie +nowy1.nazwisko);
+                }
+        }
+
